d1c8480b62b0156a49f852c1e7934231c016ebb4
enajera1 <eriknajera013@gmail.com>
2022-05-15 10:56:42 -0500

    html template updated!

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -145,7 +137,13 @@ const promptProject = portfolioData => {
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw err;
+
+      console.log('Portfolio complete! Check out index.html to see the output!');
+    });
+
   });
 
 // const printProfileData = profileDataArr => {
@@ -153,16 +151,10 @@ promptUser()
 //   for (let i = 0; i < profileDataArr.length; i += 1) {
 //     console.log(profileDataArr[i]);
 //   }
-
 //   console.log('================');
-
 //   // Is the same as this...
 //   profileDataArr.forEach(profileItem => console.log(profileItem));
 // };
-
 // printProfileData(profileDataArgs);
-
-
-
 // console.log(name, github);
 // console.log(generatePage(name, github));
